8c6e23ab7f025c49d01543aa98d152d714358ebd
Steel <lpnrhowell@gmail.com>
2021-08-07 17:48:55 -0400

    intial commit

---
+++ front-end/ryders-rally/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Search from "./components/Search";
+import Events from "./components/Events"
+
+function App() {
+	return (
+		<div className="App">
+			<Search />
+			<Events/>
+		</div>
+	);
+}
+
+export default App;
---
+++ front-end/ryders-rally/src/components/Events.js
@@ -0,0 +1,14 @@
+function events() {
+  return (
+		<div>
+			<h1>Events</h1>
+			<div
+				style={{ backgroundColor: "black", width: "200px", height: "200px" }}
+			></div>
+			<div
+				style={{ backgroundColor: "grey", width: "200px", height: "200px" }}
+			></div>
+		</div>
+	);
+}
+export default events;
