2d9d0ffb3649fff74d11be9b9092d0cccf30ab9f
Jvala2 <joevalancy@gmail.com>
2021-08-12 19:23:22 -0400

    started up the button

---
+++ front-end/ryders-rally/src/components/SharingModule.jsx
@@ -0,0 +1,16 @@
+import { gql, useQuery } from '@apollo/client';
+
+
+
+
+function Home () {
+    return(
+      <div className="homePage">
+        <Header/>
+       <Search/> 
+       facebookBtn.setAttribute("href", `https://www.facebook.com/sharer/sharer.php?u=${postURL}`);
+twitterBtn.setAttribute("href", `https://twitter.com/share?url${postURL}&text=${postTitle}`);
+      </div>
+    )
+  }
+  export default Home;
