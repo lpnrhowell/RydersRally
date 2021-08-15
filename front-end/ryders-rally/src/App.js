b48b6f6bd4f4db358463ec0aeb8ff5f4c5140029
Steel <lpnrhowell@gmail.com>
2021-08-15 01:42:43 -0400

    added conection to graphql on the front end and made styling changes

--- front-end/ryders-rally/src/App.js
+++ front-end/ryders-rally/src/App.js
@@ -1,17 +1,33 @@
-import "./App.scss";
-import Home from "./components/Home"
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
 
+import 'semantic-ui-css/semantic.min.css';
+import './App.scss';
+
+import { AuthProvider } from './context/auth';
+import AuthRoute from './helper/auth';
+
+import Nav from './components/Nav';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
 
 
 function App() {
-	
-	
-	return (
-		<div className="App">
-			<Home />
-		
-		</div>
-	);
+  return (
+    <AuthProvider>
+      <Router>
+        <Container>
+          <Nav />
+          <Route exact path="/" component={Home} />
+          <AuthRoute exact path="/login" component={Login} />
+          <AuthRoute exact path="/register" component={Register} />
+          {/* <Route exact path="/posts/:postId" component={SinglePost} /> */}
+        </Container>
+      </Router>
+    </AuthProvider>
+  );
 }
 
 export default App;
