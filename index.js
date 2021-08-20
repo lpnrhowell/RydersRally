d815dce1628861a2c044ed293602b7605927be9a
“lpnrhowell” <lpnrhowell@gmail.com>
2021-08-19 23:04:26 -0400

    disabling apollo homepage

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const { ApolloServer } = require("apollo-server");
+const { ApolloServerPluginLandingPageDisabled } = require("apollo-server-core");
 const mongoose = require("mongoose");
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
